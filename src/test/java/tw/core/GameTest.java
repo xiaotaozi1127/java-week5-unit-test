ae4d574ca7a50053bdb59b433f368d8af1c7fa6d
htao <htao@thoughtworks.com>
2018-04-18 17:57:33 +0800

    [taohui] add test for game: should generate correct guess result and status

--- src/test/java/tw/core/GameTest.java
+++ src/test/java/tw/core/GameTest.java
@@ -1,10 +1,45 @@
 package tw.core;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.generator.AnswerGenerator;
+import tw.core.model.GuessResult;
+
 /**
  * 在GameTest文件中完成Game中对应的单元测试
  */
 
-
 public class GameTest {
+    Game game;
+    @Before
+    public void setUp() throws OutOfRangeAnswerException {
+        AnswerGenerator mockGenerator = Mockito.mock(AnswerGenerator.class);
+        Mockito.when(mockGenerator.generate()).thenReturn(Answer.createAnswer("1 2 3 4"));
+        game = new Game(mockGenerator);
+    }
+
+    @Test
+    public void should_generate_correct_guess_result_for_given_guess_answer() {
+        GuessResult guessResult = game.guess(Answer.createAnswer("1 3 5 6"));
+        Assert.assertEquals("1A1B", guessResult.getResult());
+    }
+
+    @Test
+    public void should_be_success_if_guess_answer_is_correct(){
+        game.guess(Answer.createAnswer("1 2 3 4"));
+        String status = game.checkStatus();
+        Assert.assertEquals("success", status);
+    }
+
+    @Test
+    public void should_allow_continue_if_guess_number_less_than_6(){
+        game.guess(Answer.createAnswer("1 4 3 5"));
+        String status = game.checkStatus();
+        Assert.assertEquals("continue", status);
+    }
+
 
 }
