307d39ccbbb83b71d81a960fd70123fcb42f3829
htao <htao@thoughtworks.com>
2018-04-18 17:26:30 +0800

    [taohui] add test for answer generate: generate 4 numbers less than 10

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,25 @@
 package tw.core.generator;
 
+import org.junit.Assert;
+import org.junit.Test;
+import tw.core.Answer;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import java.util.Arrays;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
-
+    @Test
+    public void show_generate_4_numbers_less_than_10() throws OutOfRangeAnswerException {
+        AnswerGenerator generator = new AnswerGenerator(new RandomIntGenerator());
+        Answer answer = generator.generate();
+        String toString = answer.toString();
+        String[] split = toString.split(" ");
+        Assert.assertEquals(4, split.length);
+        boolean allMatch = Arrays.stream(split).allMatch(i -> Integer.parseInt(i) < 10);
+        Assert.assertTrue(allMatch);
+    }
 }
 
