38c60326e9292c0318c8d7f42fceb826cb7bbbd7
htao <htao@thoughtworks.com>
2018-04-20 09:09:05 +0800

    [taohui] refactor randomIntGenerator

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -10,6 +10,7 @@ import tw.core.generator.RandomIntGenerator;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
@@ -20,7 +21,7 @@ public class RandomIntGeneratorTest {
     public final ExpectedException exception = ExpectedException.none();
 
     @Test
-    public void show_throw_exception_if_need_number_larger_than_max_limit(){
+    public void show_throw_exception_if_need_number_count_larger_than_max_limit(){
         RandomIntGenerator generator = new RandomIntGenerator();
         exception.expect(IllegalArgumentException.class);
         exception.expectMessage("Can't ask for more numbers than are available");
@@ -28,7 +29,7 @@ public class RandomIntGeneratorTest {
     }
 
     @Test
-    public void show_generate_same_number_as_required_and_not_larger_than_max(){
+    public void show_generate_required_number_count_and_with_in_max_limit(){
         RandomIntGenerator generator = new RandomIntGenerator();
         String generateNums = generator.generateNums(5, 4);
         String[] split = generateNums.split(" ");
